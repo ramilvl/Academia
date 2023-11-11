c058c87089005aab024af84e97da45b3454b4003
Ramil Valiyev <ramil4360616@gmail.com>
2023-11-12 01:07:25 +0400

    Login Page has been deployed

---
+++ assets/js/login.js
@@ -0,0 +1,27 @@
+passwordImageElement = document.getElementById("passwordType");
+passwordElement = document.getElementById("password");
+forgotPasswordFrame = document.getElementById("forgotPassword");
+passwordImageElement.addEventListener("click",passwordTypeChange);
+
+function passwordTypeChange(){
+    if(passwordImageElement.src.endsWith("/assets/img/hidden.svg")){
+        passwordImageElement.src = "./assets/img/openEye.svg";
+        passwordElement.type = "text";
+    }
+    else {
+        passwordImageElement.src = "./assets/img/hidden.svg";
+        passwordElement.type = "password";
+    }
+}
+
+function changeView() {
+    var centerDiv = document.getElementsByClassName("container");
+    if (forgotPasswordFrame.style.display === "flex") {
+        forgotPasswordFrame.style.display = "none";
+        centerDiv[0].style.filter = "blur(0px)";
+
+    } else {
+        forgotPasswordFrame.style.display = "flex";
+        centerDiv[0].style.filter = "blur(5px)";
+    }
+}
